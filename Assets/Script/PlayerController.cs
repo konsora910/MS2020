2a8bd2cf0fc452bac3f7ed9faf2c0d3555aa4b37
haremori <umnfhr847>
2019-10-05 15:32:12 +0900

    基本操作、テストオブジェクトの追加

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,21 +6,62 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
+    [SerializeField] private Vector3    velocity;              // 移動方向
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        KeyBord();
+        /*
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+        */
+    }
+    // キーボード操作
+    void KeyBord()
+    {
+        // 移動
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(speed, 0f, 0f);  
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed, 0f, 0f);
+        }
+        // ものを持つ動作
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+        }
+        
+    }
+    // コントローラー操作
+    void control()
+    {
+
+    }
+    // ものを持つ離す（飛ばす？）関数
+    void takefood()
+    {
+
     }
 }
