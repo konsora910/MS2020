e844d82f0ac52e19e557b5f133a5b775d3c43858
Yuta <kojima.yuta.hal@gmail.com>
2019-10-30 14:51:26 +0900

    鍋script未完

---
+++ Assets/Script/Pot.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    [SerializeField] GameObject[] PotArray = new GameObject[3];
+    [SerializeField] bool bUse;
+
+    void Start()
+    {
+        foreach (GameObject i in PotArray)
+        {
+            PotArray = null;
+        }
+    }
+
+    void Update()
+    {
+        if (PotArray[2]==null) { bUse = true; }
+        else { bUse = false; }
+
+    }
+
+    void OnTriggerStay(Collider Collider)
+    {
+        if (bUse)
+        {
+
+
+            if (Collider.gameObject.tag == "egg")
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (PotArray[i] != null)
+                    {
+                        PotArray[i] = Collider.gameObject;
+                    }
+                }
+            }
+            if (Collider.gameObject.tag == "tmt")
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (PotArray[i] != null)
+                    {
+                        PotArray[i] = Collider.gameObject;
+                    }
+                }
+            }
+            if (Collider.gameObject.tag == "rice")
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (PotArray[i] != null)
+                    {
+                        PotArray[i] = Collider.gameObject;
+                    }
+                }
+            }
+        }
+    }
+}
