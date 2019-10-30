f28bc94fcc9bd44935a3a7861726a6e20dc29b18
HagiyamaYu <49739492+HagiyamaYu@users.noreply.github.com>
2019-10-30 15:33:46 +0900

    別々に持てるよ

---
+++ Assets/Script/Foodselect1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foodselect1 : MonoBehaviour
+{
+    public GameObject[] gameObjectArray = new GameObject[50];
+    public Vector3[] FoodPosition = new Vector3[50];
+    public Quaternion[] FoodRotation = new Quaternion[50];
+    public int allFood;
+    public int allFood2;
+    public GameObject clickedGameObject;
+    public bool bSave = false;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int count = 0;
+        foreach (Transform child in transform)
+        {
+            gameObjectArray[count] = child.gameObject;
+            count++;
+        }
+        allFood = count;
+
+        if (!bSave)
+        {
+
+            //gameObjectArray2 = gameObjectArray;
+            for (int i = 0; i < allFood; i++)
+            {
+                FoodPosition[i] = gameObjectArray[i].transform.position;
+                FoodRotation[i] = gameObjectArray[i].transform.rotation;
+            }
+            allFood2 = allFood;
+            bSave = true;
+
+        }
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < allFood; i++)
+        {
+            if (gameObjectArray[i].GetComponent<FoodController>().takeout == true)
+            {
+                player.GetComponent<PlayerController>().food = gameObjectArray[i];
+            }
+        }
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -30,9 +30,7 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         if (Input.GetKeyDown(KeyCode.B) && bFood_Take == true)
-        { 
-            Debug.Log("aaaaa");
-            
+        {   
             food.GetComponent<FoodController>().takeout = false;
             bFood_Take = false;
         }
@@ -72,19 +70,7 @@ public class PlayerController : MonoBehaviour
             Debug.Log("iiiii");
             if (bFood_Take == false)
             {
-                if (Collider.gameObject.tag == "egg")
-                {// フードタグが付いているゲームオブジェクトに当たった時
-                    food = GameObject.FindGameObjectWithTag("egg");
-                }
-                else if (Collider.gameObject.tag == "rice")
-                {// フードタグが付いているゲームオブジェクトに当たった時
-                    food = GameObject.FindGameObjectWithTag("rice");
-                }
-                else if (Collider.gameObject.tag == "tmt")
-                {// フードタグが付いているゲームオブジェクトに当たった時
-                    food = GameObject.FindGameObjectWithTag("tmt");
-                }
-                bFood_Take = true;
+                  bFood_Take = true;
             }
         }
        
