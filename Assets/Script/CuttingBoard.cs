cacc7d205bb856c11a91582ddb6acb5a8b044793
SeiyaTakasugi <seiya100dash@gmail.com>
2019-10-31 16:57:18 +0900

    まな板スクリプトの内容変更（変更前はコメントアウト済）

--- Assets/Script/CuttingBoard.cs
+++ Assets/Script/CuttingBoard.cs
@@ -4,70 +4,84 @@ using UnityEngine;
 
 public class CuttingBoard : MonoBehaviour
 {
-    public GameObject food;
+    public GameObject Food;
     public float time;
-    Collider col;
-    public string name;
-    FoodController fctr; // FoodController script の値を参照
+    //FoodController fctr; // FoodController script の値を参照
+    //　PlayerContorollerを外部参照
+    public GameObject P;
+    public PlayerController PControll;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("OnTriggerStay");
-        //Collider col;
+        P = GameObject.Find("Player");
+        PControll = P.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (time < 2)
-        {// 食材を置いたときにcutfood関数が使われる
-            CutFood();
-        }
     }
-    void CutFood()
+
+    void OnTriggerStay(Collider other)
     {
-        Debug.Log("切っている食材: " + gameObject.name);
-    }
+        //name = gameObject.name; // 接触したゲームオブジェクトの名前を格納する
+        //Debug.Log("接触している食材: " + gameObject.name);
 
-    private IEnumerator OnTriggerStay(Collider Collider)
-    {// 接触している時間が一定時間たったら
-
-        name = gameObject.name; // 接触したゲームオブジェクトの名前を格納する
-
-        Debug.Log("接触している食材: " + gameObject.name);
-
-        yield return time;
-    }
-    void OnCollisionExit(Collision collision)
-    {// 作成されたもの
-     //===============================================================================
-     /* 単体用 */
-     //
-     //===============================================================================
-        if (food = GameObject.FindGameObjectWithTag("egg"))
-        {// フードタグが付いているゲームオブジェクトに当たった時
-            Debug.Log("失敗!");
-            fctr.begg = true;
-
-
-        }
-        else if (food = GameObject.FindGameObjectWithTag("rice"))
-        {// フードタグが付いているゲームオブジェクトに当たった時
-            Debug.Log("失敗!");
-            fctr.brice = true;
-        }
-        else if (food = GameObject.FindGameObjectWithTag("tmt"))
-        {// フードタグが付いているゲームオブジェクトに当たった時
-            Debug.Log("サラダ");
-            fctr.btmt = true;
-        }
-        //===============================================================================
-        if (collision.gameObject.tag == "cook")
+        if (PControll.bFood_Take == false)
         {
-            Debug.Log("作成されたもの:" + gameObject.name);
-
+            if (other.gameObject.tag == "rice")
+            {
+                Debug.Log("米！！");
+                Food = GameObject.FindGameObjectWithTag("rice");
+                Food.GetComponent<FoodController>().takeout = false;
+                Destroy(other.gameObject);
+            }
+            if (other.gameObject.tag == "egg")
+            {
+                Debug.Log("タメェイゴォ");
+                Food = GameObject.FindGameObjectWithTag("egg");
+                Food.GetComponent<FoodController>().takeout = false;
+                Destroy(other.gameObject);
+            }
+            if (other.gameObject.tag == "tmt")
+            {
+                Debug.Log("トメェイトォウ");
+                Food = GameObject.FindGameObjectWithTag("tmt");
+                Food.GetComponent<FoodController>().takeout = false;
+                Destroy(other.gameObject);
+            }
         }
     }
+    //void OnCollisionExit(Collision collision)
+    //{// 作成されたもの
+    // //===============================================================================
+    // /* 単体用 */
+    // //
+    // //===============================================================================
+    //    if (food = GameObject.FindGameObjectWithTag("egg"))
+    //    {// フードタグが付いているゲームオブジェクトに当たった時
+    //        Debug.Log("失敗!");
+    //        //fctr = true;
+
+
+    //    }
+    //    else if (food = GameObject.FindGameObjectWithTag("rice"))
+    //    {// フードタグが付いているゲームオブジェクトに当たった時
+    //        Debug.Log("失敗!");
+    //        //fctr.brice = true;
+    //    }
+    //    else if (food = GameObject.FindGameObjectWithTag("tmt"))
+    //    {// フードタグが付いているゲームオブジェクトに当たった時
+    //        Debug.Log("サラダ");
+    //        //fctr.btmt = true;
+    //    }
+    //    //===============================================================================
+    //    if (collision.gameObject.tag == "cook")
+    //    {
+    //        Debug.Log("作成されたもの:" + gameObject.name);
+
+    //    }
+    //}
 }
