89bc9ce37aa6d19b2ef41fbc01a7ccb4c311b244
Yuta <kojima.yuta.hal@gmail.com>
2019-11-13 16:41:21 +0900

    gaugeUI

---
+++ Assets/Script/CookGaugeUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CookGaugeUI : MonoBehaviour
+{
+    public Image UIobj;
+    public bool roop;
+    public float countTime = 5.0f;
+    
+
+    void Update()
+    {
+        if (roop)
+        {
+            UIobj.fillAmount -= 1.0f / countTime * Time.deltaTime;
+        }
+    }
+}
