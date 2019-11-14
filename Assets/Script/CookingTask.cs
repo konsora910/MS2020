2f2f7a2f799f81f374713da6aa4fff7c9a7e6adc
haremori <mugimugi2984@gmail.com>
2019-11-14 15:53:33 +0900

    料理のタスクの試験

---
+++ Assets/Script/CookingTask.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingTask : MonoBehaviour
+{
+
+    public GameObject[] Task; // 料理のタスク 
+
+    // public GameObject[] CookingMat; // 素材
+    public int num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("GenerateTask"); // コルーチン　料理タスクの生成
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // 一定時間たったら料理のタスク生成
+   IEnumerator GenerateTask()
+    {
+        //3秒停止
+        yield return new WaitForSeconds(1);
+        num = Random.Range(0, Task.Length);
+        Instantiate(Task[num], transform.position, transform.rotation);
+        Debug.Log("生成したタスク:　テスト" + Task.Length);
+
+
+    }
+}
