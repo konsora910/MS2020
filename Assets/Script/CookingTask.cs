42ea6e44a7aa7591f003ea1e009baa6dc76d2f08
haremori <mugimugi2984@gmail.com>
2019-11-21 13:59:50 +0900

    no message

--- Assets/Script/CookingTask.cs
+++ Assets/Script/CookingTask.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CookingTask : MonoBehaviour
 {
-
+// タスク関係
     public GameObject[] Task; // 料理のタスク 
-
     // public GameObject[] CookingMat; // 素材
-    public int num;
+    public int num; // 乱数
+    public int MAX_task; // 表示する最大タスク
+    int task; // 今のタスク数
+// 料理の素材
+    GameObject[] food;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +21,34 @@ public class CookingTask : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     // 一定時間たったら料理のタスク生成
-   IEnumerator GenerateTask()
+    IEnumerator GenerateTask()
     {
-        //3秒停止
-        yield return new WaitForSeconds(1);
-        num = Random.Range(0, Task.Length);
-        Instantiate(Task[num], transform.position, transform.rotation);
-        Debug.Log("生成したタスク:　テスト" + Task.Length);
+        // タスクリストの最大表示数がマックスになったら
+        if (task >= MAX_task)
+        {
+            while (true)
+            {
+                num = Random.Range(0, Task.Length);
+                Instantiate(Task[num], transform.position, transform.rotation);
+                Debug.Log("生成したタスク:　テスト" + (num + 1));
+                task += 1;
+                // 10秒停止
+                yield return new WaitForSeconds(10);
 
-
+            }
+        }
+        ////////////////////////////////////////////////////////////////////
+    }
+    // 課題をクリアすると今表示したタスクを消す
+    void ResultTask()
+    {
+      //  if()
+    }
+    void ClearTask()
+    {
+        // Destroy(task);
     }
 }
