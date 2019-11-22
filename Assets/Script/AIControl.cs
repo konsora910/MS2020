b80f8a1645203b40e20ce57d38d6bdde7f750cd7
konsora <34297567+konsora910@users.noreply.github.com>
2019-11-22 12:28:06 +0900

    AI途中

--- Assets/Script/AIControl.cs
+++ Assets/Script/AIControl.cs
@@ -23,9 +23,10 @@ public class AIControl : MonoBehaviour
     public NavMeshAgent m_navAgent = null;
 
     private bool FoodHave = false;
-
     private int CookKind = 0;
     private int FoodKind = 0;
+    public GameObject food;
+    public int FoodType = 4;
     private void Awake()
     {
         m_navAgent = GetComponent<NavMeshAgent>();
@@ -41,6 +42,17 @@ public class AIControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (FoodHave == true)
+        {
+            if (FoodType == 0)
+                food.GetComponent<TomatoControl>().transform.position = new Vector3((transform.position.x ), (1), (transform.position.z));
+            else if (FoodType == 1)
+                food.GetComponent<EggControl>().transform.position = new Vector3((transform.position.x), (1), (transform.position.z));
+            else if (FoodType == 2)
+                food.GetComponent<RiceControl>().transform.position = new Vector3((transform.position.x), (1), (transform.position.z));
+
+        }
 
     }
 
@@ -59,18 +71,21 @@ public class AIControl : MonoBehaviour
                 {
                     StartCoroutine("MoveTMT");
                     CookKind = 1;
+                    FoodKind = 1;
                     yield break;
                 }
                 else if (Input.GetKey("j"))//鍋
                 {
                     StartCoroutine("MoveTMT");
                     CookKind = 2;
+                    FoodKind = 1;
                     yield break;
                 }
                 else if (Input.GetKey("k"))//フライパン
                 {
                     StartCoroutine("MoveTMT");
                     CookKind = 3;
+                    FoodKind = 1;
                     yield break;
                 }
             }
@@ -81,18 +96,21 @@ public class AIControl : MonoBehaviour
                 {
                     StartCoroutine("MoveEGG");
                     CookKind = 1;
+                    FoodKind = 2;
                     yield break;
                 }
                 else if (Input.GetKey("j"))//鍋
                 {
                     StartCoroutine("MoveEGG");
                     CookKind = 2;
+                    FoodKind = 2;
                     yield break;
                 }
                 else if (Input.GetKey("k"))//フライパン
                 {
                     StartCoroutine("MoveEGG");
                     CookKind = 3;
+                    FoodKind = 2;
                     yield break;
                 }
 
@@ -103,18 +121,21 @@ public class AIControl : MonoBehaviour
                 {
                     StartCoroutine("MoveRICE");
                     CookKind = 1;
+                    FoodKind = 3;
                     yield break;
                 }
                 else if (Input.GetKey("j"))//鍋
                 {
                     StartCoroutine("MoveRICE");
                     CookKind = 2;
+                    FoodKind = 3;
                     yield break;
                 }
                 else if (Input.GetKey("k"))//フライパン
                 {
                     StartCoroutine("MoveRICE");
                     CookKind = 3;
+                    FoodKind = 3;
                     yield break;
                 }
 
@@ -136,7 +157,8 @@ public class AIControl : MonoBehaviour
         while (true)
         {
 
-            if (FoodHave == false)
+
+            if (FoodHave== false)
             {
                 if (m_TMT != null)
                 {
@@ -151,20 +173,23 @@ public class AIControl : MonoBehaviour
                 {
                     case 1:
                         StartCoroutine("MoveCuttingBoard");
-                        break;
+                        yield break;
+                    
                     case 2:
                         StartCoroutine("MovePot");
-                        break;
+                        yield break;
+                      
                     case 3:
                         StartCoroutine("MoveFryingPan");
-
-                        break;
+                        yield break;
+                      
 
                 }
                 yield return null;
             }
                 //フレーム停止
                 yield return null;
+
 
                 //ここに再開後の処理を書く
         }
@@ -252,13 +277,14 @@ public class AIControl : MonoBehaviour
         {
             //ここに処理を書く
             if (m_FryingPan != null)
-        {
-            m_navAgent.destination = m_FryingPan.position;
+            {
+                m_navAgent.destination = m_FryingPan.position;
+            }
+            //1フレーム停止
+            yield return null;
+
+            //ここに再開後の処理を書く
         }
-        //1フレーム停止
-        yield return null;
-    
-        //ここに再開後の処理を書く
     }
     IEnumerator MovePot()
     {
@@ -283,34 +309,114 @@ public class AIControl : MonoBehaviour
         yield return null;
     }
         //ここに再開後の処理を書く
-    }
+    
 
     void OnTriggerEnter(Collider Collider)
     {// 接触中
-        if(FoodKind == 1)
-        {
-            if (Collider.gameObject.tag == "tmt")
-            {
-              Debug.Log("とまと");
+
+            if (Collider.gameObject.tag == "tmt" && FoodKind ==1)
+            { //持つ
+                if (FoodHave == false)
+                {
+                    FoodHave = true;
+                    food = Collider.gameObject;
+                    if (Collider.gameObject.tag == "tmt")
+                        FoodType = 0;
+                    if (Collider.gameObject.tag == "egg")
+                        FoodType = 1;
+                    if (Collider.gameObject.tag == "rice")
+                        FoodType = 2;
+                }
+            }
+            if (Collider.gameObject.tag == "egg" && FoodKind == 2)
+            { //持つ
+                if (FoodHave == false)
+                {
+                    FoodHave = true;
+                    food = Collider.gameObject;
+                    if (Collider.gameObject.tag == "tmt")
+                        FoodType = 0;
+                    if (Collider.gameObject.tag == "egg")
+                        FoodType = 1;
+                    if (Collider.gameObject.tag == "rice")
+                        FoodType = 2;
+                }
             }
 
-        }
-        if (FoodKind == 2)
-        {
-            if (Collider.gameObject.tag == "egg")
-            {
-                Debug.Log("たまご");
+
+            if (Collider.gameObject.tag == "rice" && FoodKind ==3)
+            { //持つ
+                if (FoodHave == false)
+                {
+                    FoodHave = true;
+                    food = Collider.gameObject;
+                    if (Collider.gameObject.tag == "tmt")
+                        FoodType = 0;
+                    if (Collider.gameObject.tag == "egg")
+                        FoodType = 1;
+                    if (Collider.gameObject.tag == "rice")
+                        FoodType = 2;
+                }
             }
 
-        }
-        if (FoodKind == 3)
-        {
-            if (Collider.gameObject.tag == "rice")
-            {
-                Debug.Log("こめー");
-            }
 
-        }
+            if (Collider.gameObject.tag == ("FP") && CookKind ==3)
+            {
+            Debug.Log("a");
+                switch (FoodKind)
+                {
+                    case 1:
+                        food.GetComponent<TomatoControl>().takeout = false;
+                        break;
+                    case 2:
+                        food.GetComponent<EggControl>().takeout = false;
+                        break;
+                    case 3:
+                        food.GetComponent<RiceControl>().takeout = false;
+                        break;
+                }
+            FoodHave = false;
+            food = null;
+            }
+        
+        //if (Collider.gameObject.tag == ("pot"))
+        //{
+        //    if (Input.GetKeyDown(KeyCode.B) && bFood_Take == true)
+        //    {
+        //        food.GetComponent<FoodController>().takeout = false;
+        //        bFood_Take = false;
+        //        PotScript.SetFood(food);
+        //    }
+        //}
 
     }
+
+    public bool TmtHave()
+    {
+        bool Check = false;
+        if(CookKind == 1)
+        {
+            Check = true;
+        }
+        return Check;
+    }
+    public bool RiceHave()
+    {
+        bool Check = false;
+        if (CookKind == 3)
+        {
+            Check = true;
+        }
+        return Check;
+    }
+    public bool EggHave()
+    {
+        bool Check = false;
+        if (CookKind == 2)
+        {
+            Check = true;
+        }
+        return Check;
+    }
+
 }
