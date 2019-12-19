b40ded799994c36abf2bf3d336f9456765bbfc66
HagiyamaYu <49739492+HagiyamaYu@users.noreply.github.com>
2019-12-19 16:44:50 +0900

    終了

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -9,33 +9,50 @@ public class Timer : MonoBehaviour
 {
 
     [SerializeField] public float Seconds = 180.9f;
+    GameObject Finish;
     public GameObject timer_object = null;
     public bool b_TimeUp = false;
+    public bool b_end = false;
     float n_timer = 0;
+    float f_count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        Seconds = 180.9f;
-        n_timer = 0;
+        Seconds = 0.9f;
+        n_timer = 0.0f;
+        f_count = 0.0f;
+        b_end = false;
         b_TimeUp = false;
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Seconds <= 0 && b_TimeUp == false)
+        if (Seconds <= 0 && b_TimeUp == false && b_end == true)
         {
             GameObject player = GameObject.FindGameObjectWithTag("Player");
             player.gameObject.GetComponent<Stop>().StopObject();
             GameObject obj = GameObject.FindGameObjectWithTag("scene");
-            if(SceneManager.GetActiveScene().name == "SinglePlayScene")
+            if (SceneManager.GetActiveScene().name == "SinglePlayScene")
                 obj.gameObject.GetComponent<SceneControl>().FadeOut("SPResultScene");
             else if (SceneManager.GetActiveScene().name == "DoublePlayScene")
                 obj.gameObject.GetComponent<SceneControl>().FadeOut("DPResultScene");
             b_TimeUp = true;
         }
-        else if(Seconds > 0)
+        else if (Seconds <= 0 && b_end == false) 
+        {
+            Finish.SetActive(true);
+            f_count += Time.deltaTime;
+            if(f_count <=0.5f)
+            {
+                Finish.transform.localScale = new Vector3(30.0f * f_count, 20.0f * f_count, 1.0f);
+            }
+            if (f_count >= 3.0f)
+                b_end = true;
+        }
+        else if (Seconds > 0)
         {
             Text timer_text;
             timer_text = timer_object.GetComponent<Text>();
@@ -45,5 +62,10 @@ public class Timer : MonoBehaviour
             Seconds -= n_timer;
         }
 
+    }
+    void Init()
+    {
+        Finish = GameObject.Find("Finish");
+        Finish.SetActive(false);
     }
 }
