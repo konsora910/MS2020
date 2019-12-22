f39f11f9e0ae1ed4c3f9f73feba401362c76ee54
haremori <umnfhr847>
2019-12-23 02:13:15 +0900

    現状できる修正

---
+++ Assets/Script/GameSetting.cs
@@ -0,0 +1,41 @@
+﻿//=============================================
+// ゲーム画面上の設定を入れる
+//　
+//
+//=============================================
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetting : MonoBehaviour
+{
+    // 画面サイズ
+    // public static int  GameScreenSize_Width; // 幅
+    // public static int  GameScreenSize_Hight; // 高さ
+    // public static bool FullScreen;          // フルスクリーンするかしないか
+    // public static int  RefreshRate;          //  リフレッシュレート数
+
+
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        // 画面サイズ
+        Screen.SetResolution(1920, 1080, false, 60);
+    }
+
+    public int ScreenWidth;
+    public int ScreenHeight;
+
+    void Awake()
+    {
+        // PC向けビルドだったらサイズ変更
+        if (Application.platform == RuntimePlatform.WindowsPlayer ||
+        Application.platform == RuntimePlatform.OSXPlayer ||
+        Application.platform == RuntimePlatform.LinuxPlayer)
+        {
+            Screen.SetResolution(ScreenWidth, ScreenHeight, false);
+        }
+    }
+}
