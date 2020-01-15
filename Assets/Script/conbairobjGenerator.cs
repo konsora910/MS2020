7c9cead1887ededfcbac1692dd9a25fb4cdcd7f5
haremori <mugimugi2984@gmail.com>
2020-01-15 20:46:39 +0900

    ステージ微調整とコンベアの追加

---
+++ Assets/Script/conbairobjGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conbairobjGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_object = null;
+    [SerializeField]
+    private float m_time = 2.0f;
+
+    void Start()
+    {
+        StartCoroutine(Generator(m_time));
+    }
+
+    private IEnumerator Generator(float i_time)
+    {
+        var waitTime = new WaitForSeconds(i_time);
+        while (true)
+        {
+            yield return waitTime;
+            Vector3 offset = Vector3.zero;
+            offset.x = Random.Range(-4.0f, 4.0f);
+            var obj = GameObject.Instantiate(m_object, transform.position + offset, Quaternion.identity) as GameObject;
+            obj.SetActive(true);
+        }
+
+    }
+}
